8584e021d45da4e00d5453282f6443763eba36f4
Valentin <valentinignaciopenna@gmail.com>
2024-12-26 19:57:32 -0300

    navbar correccion

--- src/components/primary/Navbar.tsx
+++ src/components/primary/Navbar.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className='bg-primary h-16 w-full flex items-center p-2'>
         <h1>Yo Quiero Mis Fotos</h1>
+        <div>
+            <Link to="/">Inicio</Link>
+            <Link to="/login">Login</Link>
+        </div>
     </div>
   )
 }
